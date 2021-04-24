04073e020782ae42ff9059986db02361141413c6
Junichirou Hiroto <rivowowor7@gmail.com>
2021-04-24 23:11:36 +0800

    Added: initial files

---
+++ calculator.js
@@ -0,0 +1,9 @@
+// Operator Function
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+function operate(opr, a, b) {
+    return opr(a, b);
+}
